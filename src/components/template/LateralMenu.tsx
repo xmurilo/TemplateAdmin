b7c0584964b9facbc2e88a8a50789f46e91ba3d8
Murilo <dixmurilox@gmail.com>
2024-05-01 19:38:26 -0300

    feat: adding logout button to LateralMenu

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -1,17 +1,23 @@
-import { BellIcon, HomeIcon, SettingsIcon } from "../Icons";
+import { BellIcon, HomeIcon, LogoutIcon, SettingsIcon } from "../Icons";
 import ItemMenu from "./ItemMenu";
 import Logo from "./Logo";
 
 export default function LateralMenu() {
   return (
-    <aside>
-      <div className={` flex items-center justify-center w-20 h-20 bg-gray-950`}>
+    <aside className={`flex flex-col`}>
+      <div className={` flex items-center justify-center w-24 h-20 bg-gray-950`}>
         <Logo />
       </div>
-      <ul>
+      <ul className={`flex-grow`}>
         <ItemMenu url="/" text="Inicio" icon={HomeIcon} />
         <ItemMenu url="/settings" text="Configurações" icon={SettingsIcon} />
         <ItemMenu url="/notifications" text="Notificações" icon={BellIcon} />
+      </ul>
+      <ul>
+        <ItemMenu 
+        text="Sair" icon={LogoutIcon}
+        onClick={() => console.log("Sair")}
+        className={`text-[#FD4056] hover:bg-red-700 hover:text-white`} />
       </ul>
     </aside>
   );

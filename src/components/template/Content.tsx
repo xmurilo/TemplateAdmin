7daa7569be254882ba6f70c1e7892502cc36e976
Murilo <dixmurilox@gmail.com>
2024-05-01 19:03:21 -0300

    refactor: formating the code

--- src/components/template/Content.tsx
+++ src/components/template/Content.tsx
@@ -5,10 +5,7 @@ interface ContentProps {
 export default function Content(props: ContentProps) {
   return (
     <div
-      className={`
-      flex flex-col mt-7
-    `}
-    >
+      className={`flex flex-col mt-7`}>
       {props.children}
     </div>
   );

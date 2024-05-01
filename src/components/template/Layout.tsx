af4821193be03b441ff302f004c87113a616c310
Murilo <dixmurilox@gmail.com>
2024-05-01 19:03:16 -0300

    refactor: formating the code

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -10,10 +10,7 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   return (
-    <div
-      className={`
-      flex h-screen w-screen`}
-    >
+    <div className={`flex h-screen w-screen`}>
       <LateralMenu />
       <div className={`flex flex-col w-full p-7  bg-gray-300 dark:bg-gray-900  `}>
         <Header title={props.title} subTitle={props.subTitle} />

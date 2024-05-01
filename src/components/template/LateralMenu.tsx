d39c7a941a039c3e1ed309fce47d83e45ce00dd9
Murilo <dixmurilox@gmail.com>
2024-05-01 16:07:03 -0300

    feat: adding the ItemMenu to LateralMenu

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -1,7 +1,14 @@
+import { BellIcon, HomeIcon, SettingsIcon } from "../Icons";
+import ItemMenu from "./ItemMenu";
+
 export default function LateralMenu() {
   return (
     <aside>
-      <h1>Menu</h1>
+      <ul>
+        <ItemMenu url="/" text="Inicio" icon={HomeIcon} />
+        <ItemMenu url="/settings" text="Configurações" icon={SettingsIcon} />
+        <ItemMenu url="/notifications" text="Notificações" icon={BellIcon} />
+      </ul>
     </aside>
   );
 }

ebc289c51585cb7a77d45c898de30d66bce9d316
Murilo <dixmurilox@gmail.com>
2024-05-01 19:01:58 -0300

    feat: adding the logo

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -1,9 +1,13 @@
 import { BellIcon, HomeIcon, SettingsIcon } from "../Icons";
 import ItemMenu from "./ItemMenu";
+import Logo from "./Logo";
 
 export default function LateralMenu() {
   return (
     <aside>
+      <div className={` flex items-center justify-center w-20 h-20 bg-gray-950`}>
+        <Logo />
+      </div>
       <ul>
         <ItemMenu url="/" text="Inicio" icon={HomeIcon} />
         <ItemMenu url="/settings" text="Configurações" icon={SettingsIcon} />

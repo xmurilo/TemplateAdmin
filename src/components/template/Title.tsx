33c094bab06e7b2db75e85a6e9771993c1b0ddc2
Murilo <dixmurilox@gmail.com>
2024-05-01 20:22:40 -0300

    feat: adding propertie of dark mode

--- src/components/template/Title.tsx
+++ src/components/template/Title.tsx
@@ -6,8 +6,8 @@ interface TitleProps {
 export default function Title(props: TitleProps) {
   return (
     <div>
-      <h1 className={` font-black text-3xl text-gray-900 `}>{props.title}</h1>
-      <h2 className={`font-light text-sm text-gray-600`}>{props.subTitle}</h2>
+      <h1 className={` font-black text-3xl text-gray-900 dark:text-gray-100 `}>{props.title}</h1>
+      <h2 className={`font-light text-sm text-gray-600 dark:text-gray-300`}>{props.subTitle}</h2>
     </div>
   );
 }

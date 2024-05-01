489fdee40f2d8d37c90af3288d83145a15a8c572
Murilo <dixmurilox@gmail.com>
2024-05-01 17:27:24 -0300

    feat: settings page

---
+++ src/pages/settings.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Settings() {
+  return (
+    <Layout title="Configurações" subTitle="Personalize seu sistema por aqui!">
+      <h3>Conteudo</h3>
+    </Layout>
+  );
+}

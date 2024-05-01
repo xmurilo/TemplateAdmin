489ec77a44f5c3395c16e457e3bad959806e8aa1
Murilo <dixmurilox@gmail.com>
2024-05-01 16:07:36 -0300

    feat: adding flex properties with tailwind

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -10,10 +10,15 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   return (
-    <div>
+    <div
+      className={`
+      flex h-screen w-screen`}
+    >
       <LateralMenu />
-      <Header title={props.title} subTitle={props.subTitle} />
-      <Content>{props.children}</Content>
+      <div className={`flex flex-col w-full p-7  bg-gray-300 dark:bg-gray-900  `}>
+        <Header title={props.title} subTitle={props.subTitle} />
+        <Content>{props.children}</Content>
+      </div>
     </div>
   );
 }

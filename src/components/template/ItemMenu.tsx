81abd2d696f312443e264fa9f73fd7fc73088f81
Murilo <dixmurilox@gmail.com>
2024-05-01 19:37:17 -0300

    feat: conditional rendering and onClick event to ItemMenu

--- src/components/template/ItemMenu.tsx
+++ src/components/template/ItemMenu.tsx
@@ -1,22 +1,37 @@
 import Link from "next/link";
 
 interface ItemMenuProps {
-  url: string;
+  url?: string;
   text: string;
   icon: any;
+  className?: string;
+  onClick?: (e: any) => void;
 }
 
 export default function ItemMenu(props: ItemMenuProps) {
+  function renderContent(): JSX.Element {
+    return (
+      <a
+        className={`flex flex-col items-center justify-center h-20 w-24
+        text-gray-600 ${props.className}`}
+      >
+        {props.icon}
+        <span className={`text-xs font-light `}>{props.text}</span>
+      </a>
+    );
+  }
+
   return (
-    <li className={`hover:bg-gray-100 `}>
-      <Link legacyBehavior href={props.url}>
-        <a className={`
-            flex flex-col items-center justify-center 
-            h-20 w-20`}>
-          {props.icon}
-          <span className={`text-xs font-light text-gray-600`}>{props.text}</span>
-        </a>
-      </Link>
+    <li onClick={props.onClick} className={`hover:bg-gray-100 cursor-pointer `}>
+      {props.url ? (
+        <Link legacyBehavior href={props.url}>
+          {renderContent()}
+        </Link>
+      ) : (
+        renderContent()
+      )}
     </li>
   );
 }
+
+

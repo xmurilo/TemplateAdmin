1af99f1674a330dbc5c6e2cee6e8dc687337102e
Murilo <dixmurilox@gmail.com>
2024-05-01 17:28:16 -0300

    feat: adding css and the structure

--- src/components/template/ItemMenu.tsx
+++ src/components/template/ItemMenu.tsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
+
 interface ItemMenuProps {
   url: string;
   text: string;
   icon: any;
 }
 
-export default function ItemMenu(props: ItemMenuProps){
+export default function ItemMenu(props: ItemMenuProps) {
   return (
-    <li className={``}>
-      {props.icon}
+    <li className={`hover:bg-gray-100 `}>
+      <Link legacyBehavior href={props.url}>
+        <a className={`
+            flex flex-col items-center justify-center 
+            h-20 w-20`}>
+          {props.icon}
+          <span className={`text-xs font-light text-gray-600`}>{props.text}</span>
+        </a>
+      </Link>
     </li>
-  )
+  );
 }

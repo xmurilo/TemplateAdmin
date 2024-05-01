e4f7f69dc99fe05e7e3870ce4d4810476abbe9b3
Murilo <dixmurilox@gmail.com>
2024-05-01 17:27:15 -0300

    feat: notifications page

---
+++ src/pages/notifications.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Notifications() {
+  return (
+    <Layout title="Notificações" subTitle="Aqui você ira gerenciar suas notificações">
+      <h3>Conteudo</h3>
+    </Layout>
+  );
+}

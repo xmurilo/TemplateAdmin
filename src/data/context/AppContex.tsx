1add864ebdab5afd8333e827545b1acaf7a011f9
Murilo <dixmurilox@gmail.com>
2024-05-03 15:41:49 -0300

    feat: AppContext

---
+++ src/data/context/AppContex.tsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+type Theme = "dark" | "";
+
+interface AppContextProps {
+  theme: Theme;
+  toggleTheme?: () => void;
+}
+
+const AppContext = createContext<AppContextProps>({} as AppContextProps);
+
+export function AppProvider(props: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState<Theme>("");
+
+  function toggleTheme() {
+    setTheme(theme === "" ? "dark" : "");
+  }
+
+  return <AppContext.Provider value={{ theme, toggleTheme }}>{props.children}</AppContext.Provider>;
+}
+
+export default AppContext;

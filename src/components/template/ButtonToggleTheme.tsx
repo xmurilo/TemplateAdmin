281b65b07b70d27550c2c6d9840bf7ae77fa31d5
Murilo <dixmurilox@gmail.com>
2024-05-03 20:24:10 -0300

    feat: ButtonToggleTheme component

---
+++ src/components/template/ButtonToggleTheme.tsx
@@ -0,0 +1,40 @@
+import { MoonIcon, SunIcon } from "../Icons";
+
+interface ButtonToggleThemeProps {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+export default function ButtonToggleTheme(props: ButtonToggleThemeProps) {
+  return props.theme === "dark" ? (
+    <div
+      onClick={props.toggleTheme}
+      className={`hidden sm:flex items-center cursor-pointer bg-gradient-to-r from-yellow-300 to-orange-600 w-14 lg:w-24 h-8 p-1 rounded-full `}
+    >
+      <div
+        className={` 
+      flex items-center justify-center bg-white text-orange-600 w-6 h-6 rounded-full`}
+      >
+        {SunIcon(5)}
+      </div>
+      <div className={` hidden lg:flex items-center ml-4 text-white`}>
+        <span className={`text-sm`}>Claro</span>
+      </div>
+    </div>
+  ) : (
+    <div
+      onClick={props.toggleTheme}
+      className={`hidden sm:flex items-center justify-end cursor-pointer bg-gradient-to-r from-gray-500 to-gray-900 w-14 lg:w-24 h-8 p-1 rounded-full `}
+    >
+      <div className={`hidden lg:flex items-center mr-1 text-gray-300`}>
+        <span className={`text-sm`}>Escuro</span>
+      </div>
+      <div
+        className={` 
+        flex items-center justify-center bg-black text-yellow-300 w-6 h-6 rounded-full`}
+      >
+        {MoonIcon(5)}
+      </div>
+    </div>
+  );
+}

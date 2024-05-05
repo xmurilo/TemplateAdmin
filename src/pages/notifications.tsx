3b73c3ddff3e1cfc749bee30aa2b898e15a01c31
Murilo <dixmurilox@gmail.com>
2024-05-04 21:38:27 -0300

    refactor: changing title content

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/template/Layout";
 export default function Notifications() {
   return (
     <Layout title="Notificações" subTitle="Aqui você ira gerenciar suas notificações">
-      <h3>Conteudo</h3>
+      <h3>Notificações</h3>
     </Layout>
   );
 }

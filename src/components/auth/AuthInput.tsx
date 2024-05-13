880fecf08b7c35a0214af3fbcb428584e6bf95c7
Murilo <dixmurilox@gmail.com>
2024-05-12 22:33:47 -0300

    feat: adding style to input

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -9,13 +9,15 @@ interface AuthInputProps {
 
 export default function AuthInput(props: AuthInputProps) {
   return props.notRenderWhen ? null : (
-    <div className="flex flex-col ">
+    <div className="flex flex-col mt-4 ">
       <label>{props.label}</label>
       <input
         type={props.type ?? "text"}
         value={props.value}
         onChange={e => props?.valueChanged(e.target.value)}
         required={props.obrigatory}
+        className="px-4 py-3 rounded-lg bg-gray-200 mt-2 
+        border focus:border-blue-500 focus:outline-none focus:bg-white"
       />
     </div>
   );

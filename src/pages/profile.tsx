bf96df1aa269f230974408eb3a387b92f49990e7
Murilo <dixmurilox@gmail.com>
2024-05-16 16:50:53 -0300

    feat: profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/template/Layout";
+
+
+export default function Notifications() {
+  return (
+    <Layout title="Perfil do Usuário" subTitle="Administre as suas informações de usuário !">
+      <h3>Notificações</h3>
+    </Layout>
+  );
+}

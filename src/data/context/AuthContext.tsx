6c7eb9dabe77f7e1619dce0bfb4a3d53a895f6ef
Murilo <dixmurilox@gmail.com>
2024-05-15 22:24:25 -0300

    feat: structure of AuthContext

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,2 +1,26 @@
+import { ReactNode, createContext, useState } from "react";
 import firebase from "../../firebase/config";
+import User from "@/src/model/User";
 
+interface AuthContextProps {
+  user: User | null;
+  loginGoogle: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
+
+
+
+export function AuthProvider(props: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  async function loginGoogle() {
+
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, loginGoogle }}>{props.children}</AuthContext.Provider>
+  );
+}
+
+export default AuthContext;

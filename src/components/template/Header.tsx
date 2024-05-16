3bfdf2974ab4767dfa0ad3d44bbbb1fcb6cb0cab
Murilo <dixmurilox@gmail.com>
2024-05-16 16:52:16 -0300

    feat: applying the UerAvatar in Header

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,6 +1,7 @@
 import Title from "./Title";
 import ButtonToggleTheme from "./ButtonToggleTheme";
 import useAppData from "@/src/data/hooks/useAppData";
+import UserAvatar from "./UserAvatar";
 
 interface HeaderProps {
   title: string;
@@ -12,8 +13,9 @@ export default function Header(props: HeaderProps) {
   return (
     <header className={`flex`}>
       <Title title={props.title} subTitle={props.subTitle} />
-      <div className={`flex flex-grow justify-end`}>
+      <div className={`flex flex-grow justify-end items-center`}>
         <ButtonToggleTheme theme={ctx.theme} toggleTheme={ctx.toggleTheme} />
+        <UserAvatar className="ml-3" />
       </div>
     </header>
   );

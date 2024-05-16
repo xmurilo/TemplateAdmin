8d8335d341dc3b0b62b9c34abdc51f99cb1d3af8
Murilo <dixmurilox@gmail.com>
2024-05-16 19:14:58 -0300

    feat: function to manageCookies and configureSession

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -2,7 +2,7 @@ import route from "next/router";
 import { ReactNode, createContext, useState } from "react";
 import firebase from "../../firebase/config";
 import User from "@/src/model/User";
-
+import Cookies from "js-cookie";
 interface AuthContextProps {
   user: User | null;
   loginGoogle: () => Promise<void>;
@@ -22,16 +22,40 @@ async function normalizedUser(userFirebase: firebase.User): Promise<User> {
   };
 }
 
+function manageCookies(logged: boolean) {
+  if (logged) {
+    Cookies.set("admin-template-cod3r-auth", logged.toString(), {
+      expires: 7,
+    });
+  } else {
+    Cookies.remove("admin-template-auth");
+  }
+}
+
 export function AuthProvider(props: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function configureSssion(userFirebase: firebase.User) {
+    if (userFirebase?.email) {
+      const user = await normalizedUser(userFirebase);
+      setUser(user);
+      manageCookies(true);
+      setIsLoading(false);
+      return user.email;
+    } else {
+      setUser(null);
+      manageCookies(false);
+      setIsLoading(true);
+      return false;
+    }
+  }
 
   async function loginGoogle() {
     try {
       const resp = await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
-
       if (resp.user?.email) {
-        const user = await normalizedUser(resp.user);
-        setUser(user);
+        configureSssion(resp.user);
         route.push("/");
       }
     } catch (e) {

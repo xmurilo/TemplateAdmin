c4a70bc591597c883ff343efe6088ef8ee8a37c7
Murilo <dixmurilox@gmail.com>
2024-05-17 00:07:34 -0300

    feat: Envolving the ForceAuthentication in the  Layout component

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -2,6 +2,7 @@ import useAppData from "@/src/data/hooks/useAppData";
 import Content from "./Content";
 import Header from "./Header";
 import LateralMenu from "./LateralMenu";
+import ForceAuthentication from "../auth/ForceAuthentication";
 
 interface LayoutProps {
   title: string;
@@ -12,12 +13,14 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
   const { theme } = useAppData();
   return (
-    <div className={`${theme} flex h-screen w-screen`}>
-      <LateralMenu />
-      <div className={`flex flex-col w-full p-7  bg-gray-300 dark:bg-gray-900  `}>
-        <Header title={props.title} subTitle={props.subTitle} />
-        <Content>{props.children}</Content>
+    <ForceAuthentication>
+      <div className={`${theme} flex h-screen w-screen`}>
+        <LateralMenu />
+        <div className={`flex flex-col w-full p-7  bg-gray-300 dark:bg-gray-900  `}>
+          <Header title={props.title} subTitle={props.subTitle} />
+          <Content>{props.children}</Content>
+        </div>
       </div>
-    </div>
+    </ForceAuthentication>
   );
 }
